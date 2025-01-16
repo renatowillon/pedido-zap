b6701cef686b386ae33b765968563984319a972e
“renatowillon” <renatowillon@hotmail.com>
2025-01-16 14:29:35 -0300

    feat: Add Dark Mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+
 const Page = () => {
-  return <div>HOME</div>;
+  return (
+    <div className="p-10">
+      <ThemeToggle />
+      <Button>Clique Aqui</Button>
+    </div>
+  );
 };
 export default Page;
